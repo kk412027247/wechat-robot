058260e838678124e6adef6db45532b879693679
TMD <kk412027247@gmail.com>
2020-01-13 23:34:25 +0800

    send image and video

--- wechat.py
+++ wechat.py
@@ -1,21 +1,36 @@
 import itchat
 import threading
 
+
 def send_qr_code(uuid, status, qrcode):
     with open('1.png', 'wb') as f:
         f.write(qrcode)
     print('send qr code')
 
+
 def login_success():
     print('login_success')
 
-sentences = ['aaa', 'bbbb', 'ccc', 'ddd', 'eee', 'fff', 'ggg']
+
+sentences = [
+    {'type': 'string', 'content': 'aaa'},
+    {'type': 'image', 'content': 'cat.jpg'},
+    {'type': 'string', 'content': 'ccc'},
+    {'type': 'video', 'content': 'v.mp4'},
+]
 
 itchat.auto_login(qrCallback=send_qr_code, loginCallback=login_success, hotReload=True)
 
 def sender(counter=0):
     if counter < len(sentences):
-        itchat.send(sentences[counter], toUserName='filehelper')
+        if(sentences[counter]['type'] == 'string'):
+            itchat.send(sentences[counter]['content'], toUserName='filehelper')
+        elif (sentences[counter]['type'] == 'image'):
+            itchat.send_image(sentences[counter]['content'], toUserName='filehelper')
+        elif (sentences[counter]['type'] == 'video'):
+            itchat.send_video(sentences[counter]['content'], toUserName='filehelper')
+        else:
+            pass
         counter += 1
     else:
         itchat.send('finish', toUserName='filehelper')
